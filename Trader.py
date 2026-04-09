6a9afbb3e98838ce646f2f9045e3aa0bf0c15f10
Byron Lloyd Wakeman <byzstorm1103@gmail.com>
2026-04-09 16:02:10 +0100

    [BLW] Best combo 09/04/2026 from BLW and Hamed.

--- Trader.py
+++ Trader.py
@@ -6,41 +6,42 @@ import jsonpickle
 
 class Trader:
 
-    def bid(self):
+    def bid(self) -> int:
         return 15
 
     def run(self, state: TradingState):
-        """Only method required. It takes all buy and sell orders for all
-        symbols as an input, and outputs a list of orders to be sent."""
-
-        # Data setup
-        trader_data = jsonpickle.decode(state.traderData) if state.traderData else {}
-
-        # CONSTS
-        ## Emerald
         MAX_EMERALD_POSITIONS = 20  # So we don't get burnt on one side
         EMERALD_FAIR_VAL = 10000
         EMERALD_TICK_RANGE = 3
 
-        ## Tomato
-        MAX_TOMATO_WINDOW_LENGTH = 10
-        TOMATO_STRONG_BUY_THRES = 0.4
-        TOMATO_WEAK_BUY_THRES = 0.1
-        TOMATO_STRONG_SELL_THRES = -0.4
+        MAX_TOMATO_POSITION = 10
 
+        EMERALD_MEAN = 9999.799499
 
-        # Orders to be placed on exchange matching engine
+        TOMATO_MEAN = 4992.571951219512
+        TOMATO_STD = 21.1304672973115
+
+        thres_buy = 0.067582
+        thres_sell = 0.05
+
+        buy_price_em = int(EMERALD_MEAN - 1)
+        sell_price_em = int(EMERALD_MEAN + 1)
+
+        buy_price = int(TOMATO_MEAN - 1)
+        sell_price = int(TOMATO_MEAN + 1)
+
         result = {}
-        for product in state.order_depths:
-            # Trading data setup
-            order_depth: OrderDepth = state.order_depths[product]
+        for product, order_depth in state.order_depths.items():
+            orders: List[Order] = []
+
+            current_position = state.position.get(product, 0)
 
             if product == "EMERALDS":
                 orders: List[Order] = []
 
                 # Because emerald is so stable, we can become a market maker
-                my_bid = EMERALD_FAIR_VAL - EMERALD_TICK_RANGE  # 9997 — you offer to buy here
-                my_ask = EMERALD_FAIR_VAL + EMERALD_TICK_RANGE  # 10003 — you offer to sell here
+                my_bid = EMERALD_FAIR_VAL - EMERALD_TICK_RANGE
+                my_ask = EMERALD_FAIR_VAL + EMERALD_TICK_RANGE
 
                 current_position = state.position.get(product, 0)
 
@@ -53,36 +54,31 @@ class Trader:
                     orders.append(Order(product, my_ask, -10))
                 result[product] = orders
 
-            elif product == "TOMATOES":
-                orders: List[Order] = []
-                # Strategy
-                # Mean reversion with Order Book Inbalance
+            if product == 'TOMATOES':
+                for price, quantity in order_depth.sell_orders.items():
+                    z = (price - TOMATO_MEAN) / TOMATO_STD
 
-                if "raw_imbalance" not in trader_data.keys():
-                    trader_data["raw_imbalance"] = {product: []}
+                    if z <= -thres_buy:
+                        buy_qty = min(-quantity, MAX_TOMATO_POSITION + current_position)
 
+                        if buy_qty > 0:
+                            orders.append(Order(product, price, buy_qty))
 
-                bid_vol = sum(order_depth.buy_orders.values())
-                ask_vol = sum(order_depth.sell_orders.values())  # these are negative in IMC, so abs()
-                raw_imbalance = (bid_vol - abs(ask_vol)) / (bid_vol + abs(ask_vol))
+                for price, quantity in order_depth.buy_orders.items():
+                    z = (price - TOMATO_MEAN) / TOMATO_STD
 
-                # Smoothed imbalance — store history in traderData
-                trader_data["raw_imbalance"][product].append(raw_imbalance)
-                if len(trader_data["raw_imbalance"][product]) >= MAX_TOMATO_WINDOW_LENGTH:
-                    trader_data["raw_imbalance"][product].pop(0)
+                    if z >= thres_sell:
+                        sell_qty = min(quantity, MAX_TOMATO_POSITION - current_position)
 
-                smoothed_imbalance = sum(trader_data["raw_imbalance"][product]) / len(trader_data["raw_imbalance"][product])
+                        if sell_qty > 0:
+                            orders.append(Order(product, price, -sell_qty))
 
-                best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
-                best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
+                buy_qty = MAX_TOMATO_POSITION - current_position
+                sell_qty = MAX_TOMATO_POSITION + current_position
 
-                if smoothed_imbalance > TOMATO_STRONG_BUY_THRES:  # strong buy pressure
-                    orders.append(Order(product, best_ask, 10))
-                elif smoothed_imbalance < TOMATO_STRONG_SELL_THRES:
-                    orders.append(Order(product, best_bid, -10))
+                orders.append(Order(product, buy_price, buy_qty))
+                orders.append(Order(product, sell_price, -sell_qty))
 
-                result[product] = orders
+            result[product] = orders
 
-        traderData = ""  # No state needed - we check position directly
-        conversions = 0
-        return result, conversions, traderData
+        return result, 0, ''
